5353c28a916d55df0f2141571bf818755361b647
Oleksandr Myronovych <alexmyronovych@gmail.com>
2020-08-01 20:52:17 +0300

    ini

---
+++ Parimatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parimatch
+//
+//  Created by rs on 01.08.2020.
+//  Copyright © 2020 Oleksandr Myronovych. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
