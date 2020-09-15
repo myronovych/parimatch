f4f8b4124c7454fded9ac13f986478db3df4b57a
Oleksandr Myronovych <alexmyronovych@gmail.com>
2020-09-15 10:19:31 +0300

    Refactored addDoneButton for  textField

---
+++ Parimatch/Extensions/UITextField+Ext.swift
@@ -0,0 +1,30 @@
+//
+//  UITextField+Ext.swift
+//  Parimatch
+//
+//  Created by rs on 15.09.2020.
+//  Copyright © 2020 Oleksandr Myronovych. All rights reserved.
+//
+
+import UIKit
+
+extension UITextField {
+    func addDoneButtonOnKeyboard(){
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
+
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneButtonAction))
+
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+
+        
+        inputAccessoryView = doneToolbar
+    }
+
+    @objc func doneButtonAction(){
+        resignFirstResponder()
+    }
+}
